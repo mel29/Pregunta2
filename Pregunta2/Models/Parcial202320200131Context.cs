ece5c7e4dfb5333506a2403f88329ab11f80d70d
mel29 <tvitanzo29@hotmail.com>
2023-05-20 19:33:49 -0500

    Agregar archivos de proyecto.

---
+++ Pregunta2/Models/Parcial202320200131Context.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Pregunta2.Models;
+
+public partial class Parcial202320200131Context : DbContext
+{
+    public Parcial202320200131Context()
+    {
+    }
+
+    public Parcial202320200131Context(DbContextOptions<Parcial202320200131Context> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Team> Team { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=LAPTOP-MK2FPDD2;Database=Parcial202320200131;Integrated Security=true;TrustServerCertificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Team>(entity =>
+        {
+            entity.Property(e => e.Country)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+            entity.Property(e => e.Description)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Pregunta2/Models/Team.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Pregunta2.Models;
+
+public partial class Team
+{
+    public int Id { get; set; }
+
+    public string? Description { get; set; }
+
+    public string? Country { get; set; }
+}
